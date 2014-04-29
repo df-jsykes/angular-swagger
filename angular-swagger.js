cdf641d35843cd4541dc4bc5ebcec0b429cddd5b
Jason Sykes <jasonsykes@dreamfactory.com>
2014-04-28 20:19:42 -0400

    added support for array of endpoints

--- angular-swagger.js
+++ angular-swagger.js
@@ -1,41 +1,64 @@
 'use strict';
 
 angular.module('ngSwagger', [])
-    .constant("BASE_PATH", "PATH TO YOUR API ENDPOINT")
+    .constant("BASE_PATH", "https://dsp-go.cloud.dreamfactory.com/rest")
     .constant("APP_NAME", "swagger")
     .factory('Swagger', ['$rootScope', '$http', 'BASE_PATH', 'APP_NAME', function ($rootScope, $http, basepath, appname) {
-        var Swagger = function(){
-            window.Scope = $rootScope;
+        var Swagger = {};
+        Swagger.load = function (config) {
+            //window.Scope = $rootScope;
             $http.defaults.headers.common['X-DreamFactory-Application-Name'] = appname;
-            $http.get(basepath + "/api_docs", {
-                params: { app_name: appname }
-            }).then(
-                function (response) {
-                    $rootScope.df = {};
-                    response.data.apis.forEach(function (api) {
-                        $http.get(basepath + "/api_docs" + api.path)
-                            .then(
+            if (!config) {
+                $http.get(basepath + "/api_docs", {
+                    params: { app_name: appname }
+                }).then(function (response) {
+                    Swagger.loadEndPoints(response);
+                })
+            } else {
+                config.forEach(function (endpoint) {
+                    $http.get(basepath + "/api_docs" + "/" + endpoint)
+                        .then(
                             function (response) {
-                                response.data.apis.forEach(function (api) {
-                                    var end = api.path.indexOf("/", 1);
-                                    end = end != -1 ? end : api.path.length;
-                                    var resource = api.path.slice(1, end);
-                                    $rootScope.df[resource] = $rootScope.df[resource] || {};
-                                    api.operations.forEach(function (method) {
-                                        $rootScope.df[resource][method.nickname] = function (params) {
-                                            $http({
-                                                url: basepath + "/" + resource,
-                                                method: method.method,
-                                                params: params
-                                            });
-                                        };
-                                    })
-                                })
+                                Swagger.build(response);
                             }
-                        )
-                    })
-                }
-            );
+                        );
+                });
+
+            }
+
+
         };
-        return Swagger();
+        Swagger.loadEndPoints = function (response) {
+
+            response.data.apis.forEach(function (api) {
+                $http.get(basepath + "/api_docs" + api.path)
+                    .then(
+                    function (response) {
+                        Swagger.build(response)
+                    }
+
+                )
+            })
+        };
+        Swagger.build = function (response) {
+            $rootScope.df = {};
+            response.data.apis.forEach(function (api) {
+                var end = api.path.indexOf("/", 1);
+                end = end != -1 ? end : api.path.length;
+                var resource = api.path.slice(1, end);
+                $rootScope.df[resource] = $rootScope.df[resource] || {};
+                api.operations.forEach(function (method) {
+                    $rootScope.df[resource][method.nickname] = function (params) {
+                        $http({
+                            url: basepath + "/" + resource,
+                            method: method.method,
+                            params: params
+                        });
+                    };
+                });
+            });
+        };
+
+
+        return Swagger;
     }]);
